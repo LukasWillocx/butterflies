5a391b3477805b398ad6f97f64a91af88b7a80ee
LukasW <125377952+LukasWillocx@users.noreply.github.com>
2025-04-24 12:58:13 +0200

    Shiny app functions - image prediction

---
+++ functions.R
@@ -0,0 +1,16 @@
+
+# single image prediction
+predict_image <- function(model,classes,image_path) {
+  img <- image_load(image_path, target_size = c(224, 224))
+  x <- image_to_array(img)
+  x <- array_reshape(x, c(1, dim(x)))
+  x <- x / 255  # Match training preprocessing
+  pred <- predict(model, x)
+  class_id <- which.max(pred)
+  return(list(class = classes$Name[class_id],
+              scientific_name = classes$Scientific.Name[class_id],
+              family = classes$Family[class_id],
+              location = classes$Location[class_id],
+              size_in_mm = classes$Size..mm.[class_id],
+              probability = max(pred)))
+}
